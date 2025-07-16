433812fd8157fc9cdd34cdd939c00dd1b9ca8912
ninjagaidenguy <daulvec@protonmail.com>
2025-07-15 22:09:28 -0500

    update code to be simple to update.

--- js/utils/getCharacterData.js
+++ js/utils/getCharacterData.js
@@ -1,119 +1,80 @@
 // import localforage from 'localforage';
 
-const getValue = (selector) => {
-  const element = document.querySelector(selector);
-  return element ? element.value : null;
+const getFieldValue = (selector) => {
+  const element = document.querySelector(selector)
+  return element ? element.value : ''
+}
+
+const buildSelection(mapping) {
+  const result = {}
+  for (const [key, selector] of Object.entries(mapping)) {
+    result[key] = getFieldValue(selector)
+  }
+  return result
+}
+const basicInfoMap = {
+  name: '[name="characterName"]',
+  player: '[name="player"]',
+  alignment: '[name="alignment"]',
+  currentXP: '[name="xpCurrent"]',
+  netLevel: '[name="xpNextLevel"]',
+  xpSpeed: '[name="xpSpeed"]',
+  mergedClasses: '[name="classes"]',
+  race: '[name="race"]',
+  campaign: '[name="campaign"]',
+  deity: '[name="deity"]',
+  level: '[name="level"]',
+  size: '[name="Size"]',
+  age: '[name="age"]',
+  gender: '[name="Gender"]',
+  height: '[name="height"]',
+  weight: '[name="weight"]',
+  eyes: '[name="eyes"]',
+  hair: '[name="hair"]',
 };
+
+const abilitiesMapUpdate = (type) {
+  const map = {
+    mod: `[name="${type}Mod"]`,
+    base: `[name="${type}Base"]`,
+    cost: `[name="${type}Cost"]`,
+    racial: `[name="${type}RBonus"]`,
+    bonus: `[name="${type}RBonusManual"]`,
+    levels: `[name="${type}Lvls"]`,
+    class: `[name="${type}Class"]`,
+    misc: `[name="${type}Misc"]`,
+    gear: `[name="${type}Gear"]`,
+    mythic: `[name="${type}Mythic"]`
+  } 
+ return map
+}
+const abilitiesScores = {
+  str: buildSelection(abilitiesMapUpdate('str')),
+  dex: buildSelection(abilitiesMapUpdate('dex')),
+  con: buildSelection(abilitiesMapUpdate('con')),
+  int: buildSelection(abilitiesMapUpdate('int')),
+  wis: buildSelection(abilitiesMapUpdate('wis')),
+  cha: buildSelection(abilitiesMapUpdate('cha')),
+};
+const hitpoints = {
+  total: '[name="hpMax"]',
+  curntHP: '[name="hpCurrent"]',
+  nonLethal: '[name="nonlethal"]',
+  hitDice: '[name="hd"]',
+  damageReduction: '[name="dr"]',
+}
+const armorMap = (type) {
+  const map = {
+    
+  }
+  return map
+}
 
 export const saveCharacterData = () => {
   const characterData = {
-    basicInfo: {
-      name: getValue('[name="characterName"]'),
-      player: getValue('[name="player"]'),
-      alignment: document.querySelector('[name="alignment"]').value,
-      currentXP: getValue('[name="xpCurrent"]'),
-      netLevel: getValue('[name="xpNextLevel"]'),
-      xpSpeed: document.querySelector('[name="xpSpeed"]').value,
-      mergedClasses: getValue('[name="classes"]'),
-      race: document.querySelector('[name="race"]').value,
-      campaign: getValue('[name="campaign"]'),
-      deity: getValue('[name="deity"]'),
-      level: getValue('[name="level"]'),
-      size: document.querySelector('[name="Size"]').value,
-      age: getValue('[name="age"]'),
-      gender: document.querySelector('[name="Gender"]').value,
-      height: getValue('[name="height"]'),
-      weight: getValue('[name="weight"]'),
-      eyes: getValue('[name="eyes"]'),
-      hair: getValue('[name="hair"]'),
-    },
-    abilitiesScores: {
-      str: {
-        total: getValue('[name="strTotal"]'),
-        mod: getValue('[name="strMod"]'),
-        base: getValue('[name="strBase"]'),
-        cost: getValue('[name="strCost"]'),
-        racial: getValue('[name="strRBonus"]'),
-        bonus: getValue('[name="strRBonusManual"]'),
-        levels: getValue('[name="strLvls"]'),
-        class: getValue('[name="strClass"]'),
-        misc: getValue('[name="strMisc"]'),
-        gear: getValue('[name="strGear"]'),
-        mythic: getValue('[name="strMythic"]'),
-      },
-      dex: {
-        total: getValue('[name="dexTotal"]'),
-        mod: getValue('[name="dexMod"]'),
-        base: getValue('[name="dexBase"]'),
-        cost: getValue('[name="dexCost"]'),
-        racial: getValue('[name="dexRBonus"]'),
-        bonus: getValue('[name="dexRBonusManual"]'),
-        levels: getValue('[name="dexLvls"]'),
-        class: getValue('[name="dexClass"]'),
-        misc: getValue('[name="dexMisc"]'),
-        gear: getValue('[name="dexGear"]'),
-        mythic: getValue('[name="dexMythic"]'),
-      },
-      con: {
-        total: getValue('[name="conTotal"]'),
-        mod: getValue('[name="conMod"]'),
-        base: getValue('[name="conBase"]'),
-        cost: getValue('[name="conCost"]'),
-        racial: getValue('[name="conRBonus"]'),
-        bonus: getValue('[name="conRBonusManual"]'),
-        levels: getValue('[name="conLvls"]'),
-        class: getValue('[name="conClass"]'),
-        misc: getValue('[name="conMisc"]'),
-        gear: getValue('[name="conGear"]'),
-        mythic: getValue('[name="conMythic"]'),
-      },
-      int: {
-        total: getValue('[name="intTotal"]'),
-        mod: getValue('[name="intMod"]'),
-        base: getValue('[name="intBase"]'),
-        cost: getValue('[name="intCost"]'),
-        racial: getValue('[name="intRBonus"]'),
-        bonus: getValue('[name="intRBonusManual"]'),
-        levels: getValue('[name="intLvls"]'),
-        class: getValue('[name="intClass"]'),
-        misc: getValue('[name="intMisc"]'),
-        gear: getValue('[name="intGear"]'),
-        mythic: getValue('[name="intMythic"]'),
-      },
-      wis: {
-        total: getValue('[name="wisTotal"]'),
-        mod: getValue('[name="wisMod"]'),
-        base: getValue('[name="wisBase"]'),
-        cost: getValue('[name="wisCost"]'),
-        racial: getValue('[name="wisRBonus"]'),
-        bonus: getValue('[name="wisRBonusManual"]'),
-        levels: getValue('[name="wisLvls"]'),
-        class: getValue('[name="wisClass"]'),
-        misc: getValue('[name="wisMisc"]'),
-        gear: getValue('[name="wisGear"]'),
-        mythic: getValue('[name="wisMythic"]'),
-      },
-      cha: {
-        total: getValue('[name="chaTotal"]'),
-        mod: getValue('[name="chaMod"]'),
-        base: getValue('[name="chaBase"]'),
-        cost: getValue('[name="chaCost"]'),
-        racial: getValue('[name="chaRBonus"]'),
-        bonus: getValue('[name="chaRBonusManual"]'),
-        levels: getValue('[name="chaLvls"]'),
-        class: getValue('[name="chaClass"]'),
-        misc: getValue('[name="chaMisc"]'),
-        gear: getValue('[name="chaGear"]'),
-        mythic: getValue('[name="chaMythic"]'),
-      },
-    },
-    hitPoints: {
-      total: getValue('[name="hpMax"]'),
-      curntHP: getValue('[name="hpCurrent"]'),
-      nonLethal: getValue('[name="nonlethal"]'),
-      hitDice: getValue('[name="hd"]'),
-      damageReduction: getValue('[name="dr"]'),
-    },
+    basicInfo: buildSelection(basicInfoMap),
+    abilitiesScores: abilitiesScores,
+    hitPoints: buildSelection(hitPoints),
     armorClass: {
       AC: {
         total: getValue('[name="hpMax"]'),
